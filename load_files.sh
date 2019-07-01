216565594c440979d9e0b73d57943047948af286
manzt <trevmanz94@gmail.com>
2019-07-01 18:38:40 +0100

    Add bash script to load data from NCBI

---
+++ load_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ACC=GSE92742
+PREFIX=${ACC}_Broad_LINCS
+GEO_URL=https://www.ncbi.nlm.nih.gov/geo
+
+files=(
+    Level3_INF_mlr12k_n1319138x12328.gctx
+    cell_info.txt
+    gene_info_delta_landmark.txt
+    inst_info.txt
+    pert_info.txt
+)
+
+mkdir $PREFIX
+
+for f in ${files[*]}; do
+  curl "$GEO_URL/download/?acc=$ACC&format=file&file=${PREFIX}_$f.gz" \
+    | gunzip > $PREFIX/$f
+done
